8b4a03a4bc8366b33bc2fd6f1f1c0be883da41fd
Kioko <kiokovincent12@outlook.com>
2025-09-11 01:44:57 +0300

    Update home nav for mobile

--- uniquad/src/components/Navbar.tsx
+++ uniquad/src/components/Navbar.tsx
@@ -65,7 +65,7 @@ export default function Header() {
           <div className="relative flex h-8 items-center justify-between">
             
           <div className="relative text-gray-700 dark:text-gray-300 hover:text-gray-600 dark:hover:text-gray-400" ref={dropdownRef}>
-            <Link href={'/home'} className="flex items-center gap-2 cursor-pointer hover:bg-transparent rounded-lg transition-colors scale-100 ">
+            <Link href={'/home'} className="hidden md:flex items-center gap-2 cursor-pointer hover:bg-transparent rounded-lg transition-colors scale-100 ">
               <Logo />
               <div className="font-semibold">UniQuad</div>
             </Link>
